ed9cc545894039dfbb01fff87d5f56785a8064ed
mushfequezihan <mushfeque.zihan@gmail.com>
2025-07-13 15:04:50 -0400

    testing java automation

---
+++ java_ws/JavaAutomation/src/Test/Sample.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		// Testing java codes with eclipse git workflow
+		/*
+		 * 
+		 */
+		System.out.println("I love Coffee!");
+		System.out.println("Java means Coffee LOL");
+	}
+
+}
