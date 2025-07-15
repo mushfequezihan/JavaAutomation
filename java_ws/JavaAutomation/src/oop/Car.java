87c50ab77078e2716c0259e82dc0950a8d14b078
mushfequezihan <mushfeque.zihan@gmail.com>
2025-07-14 20:13:15 -0400

    Constructor example with classes

---
+++ java_ws/JavaAutomation/src/oop/Car.java
@@ -0,0 +1,92 @@
+package oop;
+
+public class Car {
+
+	private String color;
+	private String make;
+	private String model;
+	private int year;
+	private int speed;
+	private int gear;
+	
+	// default/assigned constructor
+	public Car() {
+		this(60,4);			//constructor call within constructor
+		this.speed = speed;
+		this.gear = gear;	
+		System.out.println("constructor Without arguments");
+	}
+	
+	public Car(int speed, int gear) {
+		this.speed = speed;
+		this.gear = gear;
+		System.out.println("constructor WITH arguments");
+	}
+
+	
+	
+	//custom method
+	public void increasesSpped() {
+		System.out.println("Increasing the SPEED!");
+	}
+
+	
+	
+	//setters and getters
+	public String getColor() {
+		return color;
+	}
+
+	// this keyword reference to class variable 
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		
+		//adding validation
+		if (year > 2010) {
+			this.year = year;
+		} else {
+			System.out.println("not valid - Select newer years!");
+		}
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getGear() {
+		return gear;
+	}
+
+	public void setGear(int gear) {
+		this.gear = gear;
+	}
+	
+	
+}
---
+++ java_ws/JavaAutomation/src/oop/Classes.java
@@ -0,0 +1,50 @@
+package oop;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		
+		// create and initialize an object from class Car
+		Car car = new Car();
+		car.increasesSpped();
+		
+		//accessing setter and getters
+		car.setMake("BMW");
+		System.out.println(car.getMake());
+		System.out.println("");
+		
+		//model is not assigned getting default value
+		System.out.println("model of BMW: " + car.getModel());
+		System.out.println("");
+		
+		Car car1 = new Car();
+		car1.setMake("M-Benz");
+		System.out.println("car make: " + car1.getMake());
+		
+		car1.setModel("Benz-300");
+		System.out.println("model: " + car1.getModel());
+		
+		//select year from 2010 as validation in Car class
+		car1.setYear(2000);
+		System.out.println("year: " + car1.getYear());
+		System.out.println("");
+		
+		//constructor example
+		Car c1 = new Car();
+		c1.setMake("Honda");
+		System.out.println("car make: " + c1.getMake());
+		System.out.println("speed: " + c1.getSpeed());
+		System.out.println("gear: " + c1.getGear());
+		System.out.println("");
+		
+		Car c2 = new Car(10, 1);
+		c2.setMake("tesla");
+		System.out.println("car make: " + c2.getMake());
+		System.out.println("speed: " + c2.getSpeed());
+		System.out.println("gear: " + c2.getGear());
+		System.out.println("");
+		
+
+	}
+
+}
